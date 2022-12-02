8ae6a79c2e010f4865f8dc7cb77494e8cda0ccd4
andrey <harley3689@gmail.com>
2022-12-02 13:52:30 +0300

    "first commit"

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,18 +1,92 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
 public class BooleanSearchEngine implements SearchEngine {
-    //???
+    Map<String, List<PageEntry>> answer = new HashMap<>();
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
-        // прочтите тут все pdf и сохраните нужные данные,
-        // тк во время поиска сервер не должен уже читать файлы
+
+        if (pdfsDir.listFiles() != null) {
+            for (File pdf : pdfsDir.listFiles()) {
+                var doc = new PdfDocument(new PdfReader(pdf));
+
+                for (int i = 1; i <= doc.getNumberOfPages(); i++) {
+                    var text = PdfTextExtractor.getTextFromPage(doc.getPage(i));
+                    var words = text.split("\\P{IsAlphabetic}+");
+
+                    Map<String, Integer> freqs = new HashMap<>();
+                    for (var word : words) {
+                        if (word.isEmpty()) {
+                            continue;
+                        }
+                        word = word.toLowerCase();
+                        freqs.put(word, freqs.getOrDefault(word, 0) + 1);
+                    }
+
+                    for (Map.Entry<String, Integer> pair : freqs.entrySet()) {
+                        var word = pair.getKey();
+                        var count = pair.getValue();
+                        List<PageEntry> list = answer.getOrDefault(word, new ArrayList<>());
+                        list.add(new PageEntry(pdf.getName(), i, count));
+                        list.sort(Collections.reverseOrder());
+                        answer.put(word, list);
+                    }
+                }
+            }
+        }
     }
 
     @Override
     public List<PageEntry> search(String word) {
-        // тут реализуйте поиск по слову
-        return Collections.emptyList();
+        List<PageEntry> listAnswer = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("stop-ru.txt"))) {
+            String str;
+            List<String> stopWords = new ArrayList<>();
+            while ((str = reader.readLine()) != null) {
+                stopWords.add(str);
+            }
+
+            String[] words = word.split("\\P{IsAlphabetic}+");
+            List<String> newWords = new ArrayList<>();
+            for (String s : words) {
+                if (!stopWords.contains(s)) {
+                    newWords.add(s);
+                }
+            }
+            listAnswer.addAll(answer.get(newWords.get(0)));
+            for (int i = 1; i < newWords.size(); i++) {
+                List<PageEntry> listWordTwo = new ArrayList<>(answer.get(newWords.get(i)));
+                for (PageEntry pageWordTwo : listWordTwo) {
+                    int counter = 0;
+                    for (PageEntry pageAnswer : listAnswer) {
+                        if (pageAnswer.samePage(pageWordTwo)) {
+                            String pdfName = pageAnswer.getPdfName();
+                            int page = pageAnswer.getPage();
+                            int count = pageAnswer.getCount() + pageWordTwo.getCount();
+                            listAnswer.remove(pageAnswer);
+                            listAnswer.add(new PageEntry(pdfName, page, count));
+                            counter++;
+                            break;
+                        }
+                    }
+                    if (counter == 0) {
+                        listAnswer.add(pageWordTwo);
+                    }
+                }
+            }
+            listAnswer.sort(Collections.reverseOrder());
+
+
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return listAnswer;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,33 @@
-import java.io.File;
-import java.util.Arrays;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
+    private static final int PORT = 8989;
+
     public static void main(String[] args) throws Exception {
         BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
-        System.out.println(engine.search("бизнес"));
 
-        // здесь создайте сервер, который отвечал бы на нужные запросы
-        // слушать он должен порт 8989
-        // отвечать на запросы /{word} -> возвращённое значение метода search(word) в JSON-формате
+        try (ServerSocket serverSocket = new ServerSocket(PORT)){
+            while (true) {
+                try (Socket socket = serverSocket.accept();
+                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                     PrintWriter out = new PrintWriter(socket.getOutputStream());){
+
+                    System.out.println("Connection!");
+                    String word = in.readLine();
+
+                    Gson gson = new GsonBuilder().create();
+                    String jsonList = gson.toJson(engine.search(word));
+                    out.println(jsonList);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Don't start!");
+            e.printStackTrace();
+        }
     }
 }
